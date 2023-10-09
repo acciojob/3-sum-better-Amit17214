afdabcd8586a62d744cbbf8fd7bf17a521bc759e
Amit17214 <120364136+Amit17214@users.noreply.github.com>
2023-10-09 22:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 function threeSum(arr, target) {
-// write your code here
-	const S = [-1, 2, 1, -4];
-	const target = 1;
-	console.log(threeSum(S, target)); // Output: 2 (-1 + 2 + 1 = 2)
-
+  // Sort the input array in ascending order.
+  arr.sort((a, b) => a - b);
   
+  let closestSum = Infinity; // Initialize closestSum to a large value.
+  
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1; // Initialize the left pointer.
+    let right = arr.length - 1; // Initialize the right pointer.
+    
+    while (left < right) {
+      const currentSum = arr[i] + arr[left] + arr[right];
+      
+      // Update closestSum if the current sum is closer to the target.
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+      
+      if (currentSum < target) {
+        left++; // Move the left pointer to the right.
+      } else {
+        right--; // Move the right pointer to the left.
+      }
+    }
+  }
+  
+  return closestSum;
 }
 
 module.exports = threeSum;
