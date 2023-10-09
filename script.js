caf276ac3e095a3432cb78af1c6f6c2fd643e1bf
Amit17214 <120364136+Amit17214@users.noreply.github.com>
2023-10-09 22:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function threeSum(arr, target) {
 // write your code here
+	const S = [-1, 2, 1, -4];
+	const target = 1;
+	console.log(threeSum(S, target)); // Output: 2 (-1 + 2 + 1 = 2)
+
   
 }
 
